8a32b0e03679090831301a2ffadfd3b49d5c5ea7
Tennis Smith <tennis.n.smith@gmail.com>
2022-02-10 12:12:22 -0600

    script to turn everything off

--- test/all-off.py
+++ test/all-off.py
@@ -1,4 +1,4 @@
 import board
 import neopixel
 pixels = neopixel.NeoPixel(board.D18, 7)
-pixels.fill((255, 255, 255))
+pixels.fill((0, 0, 0))
--- test/one-led.py
+++ test/one-led.py
@@ -1,4 +1,4 @@
 import board
 import neopixel
-pixels = neopixel.NeoPixel(board.D18, 10)
+pixels = neopixel.NeoPixel(board.D18, 1)
 pixels[0] = (255, 0, 0)
