27d3b209c3b82b75538bebebce22967d1d206f2f
Tennis Smith <tennis.n.smith@gmail.com>
2022-02-13 00:27:42 -0600

    light detection

---
+++ test/dark.py
@@ -0,0 +1,20 @@
+import board
+import RPi.GPIO as GPIO
+import time
+
+channel = 20
+GPIO.setmode(GPIO.BCM)  
+# Setup your channel
+GPIO.setup(channel, GPIO.OUT)
+GPIO.output(channel, GPIO.LOW)
+
+while True:
+    # To test the value of a pin use the .input method
+    channel_is_on = GPIO.input(channel)  # Returns 0 if OFF or 1 if ON
+    
+    if channel_is_on:
+        print("ON!")
+        # Do something here
+    else:
+        print("off")
+    time.sleep(1)
